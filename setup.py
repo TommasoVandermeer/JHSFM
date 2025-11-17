acf2946aebf117c9426d7d914c29c003bd825250
TommasoVandermeer <tommasovdm@gmail.com>
2025-11-17 09:56:15 +0100

    add requirements.txt

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
+import os
+
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+    jhsfm_requirements = f.read().splitlines()
 
 setup(
     name='jhsfm',
     version='0.0.1',
     packages = find_packages(),
-    install_requires = [
-        'jax',
-        'matplotlib',
-    ]
+    install_requires = jhsfm_requirements,
 )
